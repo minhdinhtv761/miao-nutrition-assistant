6a38cebfc3940b4bf8bcdf807c348594d4430dd5
minhman727 <62245831+minhman727@users.noreply.github.com>
2022-04-15 17:53:45 +0700

    add RESTful API for accounts

--- server/src/routers/account_router.py
+++ server/src/routers/account_router.py
@@ -1,5 +1,6 @@
-from src.controller.account_controller import AccountById, ListAccount
+from src.controller.account_controller import AccountById, Auth, ListAccount
 
 def add_account_resrouce(api):
+    api.add_resource(Auth, "/auth")
     api.add_resource(ListAccount, "/account")
     api.add_resource(AccountById, "/account/<_id>")
