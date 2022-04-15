10cd7199ffc5d8f986f71d638f06297e6ceac3e4
minhman727 <62245831+minhman727@users.noreply.github.com>
2022-04-15 15:58:31 +0700

    format response (json)

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import json
+from flask import Flask, make_response
 from flask_restful import Api
 from src.connection.mongodb_setup import mongodb_setup
 from src.models.account_model import Account
@@ -8,6 +9,19 @@ def create_app():
     # Make use of flask and flask_restful
     app = Flask(__name__)
     api = Api(app)
+
+    # Format response (json)
+    @api.representation('application/json')
+    def output_json(data, code, headers=None):
+        if type(data) == str:
+            resp = make_response({'response' : json.loads(data)}, code)
+        elif type(data) == dict:
+            resp = make_response({'response' : json.loads(json.dumps(data))}, code)
+        else:
+            resp = make_response({'response' : json.loads(data.to_json())}, code)
+            
+        resp.headers.extend(headers or {})
+        return resp
 
     # Call function to connect database
     mongodb_setup()
