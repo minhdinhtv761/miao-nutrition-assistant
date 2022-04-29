42659a84eef789c06594e0db6e3dfb9ee623d198
minhman727 <62245831+minhman727@users.noreply.github.com>
2022-04-29 14:49:48 +0700

    update data model

--- server/src/models/embedded_models/method_step_embedded_model.py
+++ server/src/models/embedded_models/method_step_embedded_model.py
@@ -2,7 +2,7 @@ from mongoengine import *
 
 class MethodStep(EmbeddedDocument):
     description = StringField(require=True)
-    images = ListField(ImageField(), null=True)
+    images = ListField(ImageField(db_alias="miao"), null=True)
 
     meta = {
         "db_alias": "miao",
