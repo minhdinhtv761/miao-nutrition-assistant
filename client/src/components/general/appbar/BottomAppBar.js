60096fb60a987825a96f720f7a524063d10ceab4
Thao <19520280@gm.uit.edu.vn>
2022-05-04 12:59:31 +0700

    Fix BottomAppBar

--- client/src/components/general/appbar/BottomAppBar.js
+++ client/src/components/general/appbar/BottomAppBar.js
@@ -1,21 +1,19 @@
-import React from "react";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 import {
-  NativeBaseProvider,
-  Icon,
   Box,
-  Pressable,
-  HStack,
   Center,
-  Text,
+  HStack,
+  Icon,
   IconButton,
+  NativeBaseProvider,
+  Pressable,
+  Text,
 } from "native-base";
-import {
-  MaterialCommunityIcons,
-  AntDesign,
-} from "@expo/vector-icons";
+
 import Colors from "../../../styles/colors";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import React from "react";
 import { headerHeight } from "../../../constants/sizes";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export const BottomAppBar = (props) => {
   const [selected, setSelected] = React.useState(1);
@@ -24,119 +22,104 @@ export const BottomAppBar = (props) => {
     console.log("long");
   };
   return (
-    <NativeBaseProvider
-    position="fixed"
-    >
-        <Center flex={1}></Center>
-        <HStack 
-        
-        bg="white" 
-        alignItems="center" 
-        pb="5%" 
-        shadow={6}
-        >
-          <Pressable
-            opacity={selected === 0 ? 1 : 0.5}
-            py="2"
-            flex={1}
-            onPress={() => setSelected(0)}
-            focused
-          >
-            <Center>
-              <Icon
-                mb="1"
-                as={<MaterialCommunityIcons name="home-outline" />}
-                color="black"
-                size="sm"
-              />
-              <Text color="black" fontSize="12">
-                Trang chủ
-              </Text>
-            </Center>
-          </Pressable>
+    <HStack bg="white" alignItems="center" shadow={6}>
+      <Pressable
+        opacity={selected === 0 ? 1 : 0.5}
+        py="2"
+        flex={1}
+        onPress={() => setSelected(0)}
+        focused
+      >
+        <Center>
+          <Icon
+            mb="1"
+            as={<MaterialCommunityIcons name="home-outline" />}
+            color="black"
+            size="sm"
+          />
+          <Text color="black" fontSize="12">
+            Trang chủ
+          </Text>
+        </Center>
+      </Pressable>
 
-          <Pressable
-            opacity={selected === 1 ? 1 : 0.5}
-            py="2"
-            flex={1}
-            onPress={() => setSelected(1)}
-          >
-            <Center>
-              <Icon
-                mb="1"
-                as={<MaterialCommunityIcons name="silverware-fork-knife" />}
-                color="black"
-                size="sm"
-              />
-              <Text color="black" fontSize="12">
-                Thực đơn
-              </Text>
-            </Center>
-          </Pressable>
+      <Pressable
+        opacity={selected === 1 ? 1 : 0.5}
+        py="2"
+        flex={1}
+        onPress={() => setSelected(1)}
+      >
+        <Center>
+          <Icon
+            mb="1"
+            as={<MaterialCommunityIcons name="silverware-fork-knife" />}
+            color="black"
+            size="sm"
+          />
+          <Text color="black" fontSize="12">
+            Thực đơn
+          </Text>
+        </Center>
+      </Pressable>
 
-          <Pressable
-            py="2"
-            flex={1}
-          >
-            <Center>
-              <IconButton
-                variant="solid"
-                borderRadius="full"
-                bg={Colors.normalColor}
-                size="lg"
-                _icon={{
-                  as: AntDesign,
-                  name: "plus",
-                  color: "white",
-                }}
-                _pressed={{
-                  bg: Colors.normalColor,
-                  opacity: 0.5,
-                }}
-                onPress={onPress}
-              />
-            </Center>
-          </Pressable>
+      <Pressable py="2" flex={1}>
+        <Center>
+          <IconButton
+            variant="solid"
+            borderRadius="full"
+            bg={Colors.normalColor}
+            size="lg" //Sửa nhỏ thôi
+            _icon={{
+              as: AntDesign,
+              name: "plus",
+              color: "white",
+            }}
+            _pressed={{
+              bg: Colors.normalColor,
+              opacity: 0.5,
+            }}
+            onPress={onPress}
+          />
+        </Center>
+      </Pressable>
 
-          <Pressable
-            opacity={selected === 3 ? 1 : 0.6}
-            py="2"
-            flex={1}
-            onPress={() => setSelected(3)}
-          >
-            <Center>
-              <Icon
-                mb="1"
-                as={<MaterialCommunityIcons name="book-open-variant" />}
-                color="black"
-                size="sm"
-              />
-              <Text color="black" fontSize="12">
-                Kế hoạch
-              </Text>
-            </Center>
-          </Pressable>
+      <Pressable
+        opacity={selected === 3 ? 1 : 0.6}
+        py="2"
+        flex={1}
+        onPress={() => setSelected(3)}
+      >
+        <Center>
+          <Icon
+            mb="1"
+            as={<MaterialCommunityIcons name="book-open-variant" />}
+            color="black"
+            size="sm"
+          />
+          <Text color="black" fontSize="12">
+            Kế hoạch
+          </Text>
+        </Center>
+      </Pressable>
 
-          <Pressable
-            opacity={selected === 4 ? 1 : 0.5}
-            py="2"
-            flex={1}
-            onPress={() => setSelected(4)}
-          >
-            <Center>
-              <Icon
-                mb="1"
-                as={<MaterialCommunityIcons name="account-outline" />}
-                color="black"
-                size="sm"
-              />
-              <Text color="black" fontSize="12">
-                Cá nhân
-              </Text>
-            </Center>
-          </Pressable>
-        </HStack>
-
-    </NativeBaseProvider>
+      <Pressable
+        opacity={selected === 4 ? 1 : 0.5}
+        py="2"
+        flex={1}
+        onPress={() => setSelected(4)}
+      >
+        <Center>
+          <Icon
+            mb="1"
+            as={<MaterialCommunityIcons name="account-outline" />}
+            color="black"
+            size="sm"
+          />
+          <Text color="black" fontSize="12">
+            Cá nhân
+          </Text>
+        </Center>
+      </Pressable>
+    </HStack>
   );
 };
