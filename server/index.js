aca40b5f1e21b5c9356a22537fb4d05d6a7177ad
minhman727 <62245831+minhman727@users.noreply.github.com>
2022-06-13 19:04:41 +0700

    server init

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const database = process.env.DATABASE;
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+const databaseURI = `mongodb+srv://${username}:${password}@miao.r1oin.mongodb.net/${database}?retryWrites=true&w=majority`;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cors());
+
+mongoose
+  .connect(databaseURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+    app.listen(port, host, () => {
+      console.log(`Server is running on ${host}/${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
