5b936061a55e36296d513b9757362d6dfbd349db
Thao <19520280@gm.uit.edu.vn>
2022-06-15 05:30:42 +0700

    update call API, calc in client

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,37 +1,34 @@
 import { UserModel } from "../models/user.model.js";
 
-export const getUserByAccountId = async (req, res) =>{
-
-}
-
-export const updateUser = async (req, res) => {
+export const getUserByAccountId = async (req, res) => {
   try {
-    const user = req.body;
+    const {id} = req.params;
 
-    const updatedUser = await UserModel.findByIdAndUpdate({ _id: user._id }, user, {
-      new: true,
-    });
+    const user = await UserModel.findOne({accountId: id});
+    // const user = await UserModel.find();
 
-    return res.status(200).json(updatedUser);
-    
+    return res.status(200).json(user);
+
   } catch (error) {
     return res.status(500).json({ error: error });
   }
 };
 
-// const age = new Date().getFullYear() - user.birthday.getFullYear();
-  
-// const bmi = bmiCaculator(weight, height);
-// const bmr = bmrCalculator(percentBodyFat);
-// const tdee = tdeeCalculator(bmr, user.activity);
+export const updateUser = async (req, res) => {
+  try {
+    const user = req.body;
 
-// let newBodyComposition = {
-//   recordDate: new Date(),
-//   weight: weight,
-//   height: height,
-//   percentBodyFat: percentBodyFat,
-//   BMI: bmi,
-//   BMR: bmr,
-//   TDEE: tdee,
-//   activity: user.activity,
-// }
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      { _id: user._id },
+      user,
+      {
+        new: true,
+      }
+    );
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+};
+
